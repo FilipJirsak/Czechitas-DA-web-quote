158c9717f74ea8ff24e5b03cd5f54e719dcbd50c
Filip Jirsák <filip@jirsak.org>
2020-10-21 20:19:38 +0200

    first commit

---
+++ src/Quote/index.js
@@ -0,0 +1,10 @@
+import './styly.css';
+
+export const Bures = (props) => {
+    return `
+    <figure class="quote">
+        <blockquote>Jsme schopný národ, jem nám vládnou nemehla.</blockquote>
+        <figcaption>Andrej Babiš</figcaption>
+    </figure>
+    `;
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {Bures} from './Quote/index.js'
+import './index.html';
+import './styly.css';
+
+document.querySelector('#app').innerHTML = Bures();
